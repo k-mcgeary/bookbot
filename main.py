f446e0d3cd4285e98842b5a6f0c36b4b77de7b2d
k-mcgeary <kevin_mcgeary@yahoo.com>
2025-03-02 21:32:07 -0500

    added sort_dict function to stats .py and added report to main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from stats import get_num_words, character_count
+from stats import get_num_words, character_count, sort_dict
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text)
     char_count = character_count(text)
-    print(f"{num_words} words found in the document")
-    print(char_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    rejected_symbols = []
+
+    for char in sort_dict(char_count):
+        if char['char'].isalpha():
+            print(f"{char['char']}: {char['count']}")
+        else:
+            rejected_symbols.append(f"{char['char']}:{char['count']}")
+
+    print("============= END ===============")
+
+    
+    
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def character_count(text):
         else:
             char_count[char.lower()] = 1
     return char_count
+
+def sort_dict(char_count):
+    dict_list = []
+
+    for key, value in char_count.items():
+        dict_list.append({'char':key, 'count':value})
+    
+    dict_list.sort(key=lambda item: item['count'], reverse=True)
+
+    return dict_list        
